aaf8fce317b2d0498a542e4cf11b35cc2032b551
mue864 <mumurapa@gmail.com>
2024-09-15 21:37:29 +0200

    added icon in label

--- mortgage-repayment-calculator-main/assets/js/app.js
+++ mortgage-repayment-calculator-main/assets/js/app.js
@@ -1,8 +1,13 @@
 function changeRadioColor(radio) {
-    document.querySelectorAll('.radio_container').forEach(container => {
-        container.classList.remove('selected');
-    });
-
-    radio.parentElement.classList.add('selected');
-}
-
+    // Find the currently selected container (if any) and remove the 'selected' class
+    const containerRemove = document.querySelector('.selected');
+ 
+    if (containerRemove !== null) {
+       containerRemove.classList.remove('selected');
+    }
+ 
+    // Add the 'selected' class to the parent div of the clicked radio button
+    const containerChange = radio.closest('.radio_container');
+    containerChange.classList.add('selected');
+ }
+ 
